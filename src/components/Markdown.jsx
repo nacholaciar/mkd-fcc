b8937d07fc48730cc11a83d2a4ed760cb9954583
Ignacio <81566680+nacholaciar@users.noreply.github.com>
2023-07-25 20:00:46 -0300

    @reduxjs/toolkit

--- src/components/Markdown.jsx
+++ src/components/Markdown.jsx
@@ -1,29 +1,35 @@
-import React from "react";
-import { connect } from "react-redux";
-import { updateContent } from "../redux/actions";
-import Editor from "./Editor";
-import Preview from "./Preview";
+import React from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { updateContent } from '../redux/contentSlice';
+import Editor from './Editor';
+import Preview from './Preview';
 
-const Markdown = ({ content, updateContent }) => (
-  <div className="App d-flex flex-column vh-100 container bg-light rounded p-4">
-    <div className="d-flex flex-row h-100 overflow-auto">
-      <div
-        className="flex-grow-1 h-100 overflow-auto p-2"
-        style={{ flexBasis: "50%" }}
-      >
-        <Editor content={content} onContentChange={updateContent} />
-      </div>
-      <div
-        className="flex-grow-1 h-100 overflow-auto p-2"
-        style={{ flexBasis: "50%" }}
-      >
-        <Preview content={content} />
+const Markdown = () => {
+  const dispatch = useDispatch();
+  const content = useSelector(state => state.content);
+
+  const handleContentChange = newContent => {
+    dispatch(updateContent(newContent));
+  };
+
+  return (
+    <div className="App d-flex flex-column vh-100 container bg-light rounded p-4">
+      <div className="d-flex flex-row h-100 overflow-auto">
+        <div
+          className="flex-grow-1 h-100 overflow-auto p-2"
+          style={{ flexBasis: "50%" }}
+        >
+          <Editor content={content} onContentChange={handleContentChange} />
+        </div>
+        <div
+          className="flex-grow-1 h-100 overflow-auto p-2"
+          style={{ flexBasis: "50%" }}
+        >
+          <Preview content={content} />
+        </div>
       </div>
     </div>
-  </div>
-);
+  );
+};
 
-const mapStateToProps = (state) => ({ content: state.content });
-const mapDispatchToProps = { updateContent };
-
-export default connect(mapStateToProps, mapDispatchToProps)(Markdown);
+export default Markdown;
